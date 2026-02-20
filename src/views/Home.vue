8afe04c44267040f2f84465842554646ada45fff
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-02-20 15:48:51 +0100

    Correction on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
   This super duper website is here to help you complete your ACNH collections.
   <br>
   <br>
-  Feel free to star the project on github : https://github.com/AdrienVerstrepen/ACNH
+  Feel free to star the project on <a href="https://github.com/AdrienVerstrepen/ACNH">GitHub</a>.
 </template>
 
 <style scoped></style>
